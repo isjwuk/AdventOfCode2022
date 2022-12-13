c7f0383f9b52e5ebfe0d8eaaab7320878a2dc9a5
isjwuk <23340053+isjwuk@users.noreply.github.com>
2022-12-13 12:06:12 +0000

    Part 2 done

--- day12/README.md
+++ day12/README.md
@@ -8,13 +8,19 @@
 * [Example input file](./exampleinput.txt)
 
 ### Puzzle Description
-
+#TODO
 
 
 ### My Solution
 #### Part 1
 #TODO
-* [Dijkstra's algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+This method is very loosely based on the ideas of [Dijkstra's algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
 
 #### Part 2
+
+First thought here would be to simply wrap part 1 in another loop, using each of the letters "a" (and "S") as a starting point. However, in my input file there are
+1135 "a" characters, so this would be *very* time consuming to run.
+
+But the idea I chose is to simply start at E (the top of the hill), find the quickest way downhill to an "a". As everything is reversed we can now only step down 1 level, or across to the same level, but can step up 1 or more.
+
 #TODO
--- day12/day12-part2.ps1
+++ day12/day12-part2.ps1
@@ -4,7 +4,7 @@
 #https://adventofcode.com/2022/day/12
 
 #Read in our personal file
-$inputFile=Get-Content "./exampleinput.txt"
+$inputFile=Get-Content "./input.txt"
 #We can read the letter from coordinates with $inputFile[$y][$x]
 
 $shortestPath=@{} #Hashtable to store current shortest path. Key is coordinates
@@ -12,9 +12,11 @@ $shortestPath=@{} #Hashtable to store current shortest path. Key is coordinates
 #Find start position
 for($x=0;$x -lt $inputFile[0].Length;$x++){
     for($y=0;$y -lt $inputFile.Count;$y++){
-        if ($inputFile[$y][$x] -ceq "S"){
+        if ($inputFile[$y][$x] -ceq "E"){
             $currentX=$x
             $currentY=$y
+            #Replace the "E" with a "z"
+            $inputFile[$currenty]=$inputfile[$currenty].Substring(0,$currentx)+"z"+$inputfile[$currenty].Substring($currentx+1)
         }
     }
 }
@@ -45,84 +47,76 @@ function setShortestPath([int]$X,[int]$Y,[int]$distance){
 }
 
 
-function stepTo ([int]$X,[int]$Y,[string]$path){
+function stepTo ([int]$X,[int]$Y){
     #"Looking for routes from ("+$X+","+$Y+")" #Useful debugging line to see where search is leading
     $currentChar=[byte][char]$inputFile[$y][$x]
-    #Keep track of our route
-    $path+=[string]$X+","+ [string]$Y+"|" 
     #See if our end is next to us and at a reachable height
     if(
-        ($inputFile[$y][$x] -eq "y" -or  $inputFile[$y][$x] -eq "z") -and (
-        ($X -gt 0 -and $inputFile[$y][$x-1] -eq "E") -or
-        ($X -lt $inputFile[0].length -and $inputFile[$y][$x+1] -eq "E") -or
-        ($y -lt $inputFile.Count-1 -and $inputFile[$y+1][$x] -eq "E") -or
-        ($y -gt 0 -and $inputFile[$y-1][$x] -eq "E"))
-        ){
-            #Reached the end, no point looking for more steps down this route
-            $EndSteps=[int](getShortestPath $x $y)+1
-            "Reached the end in "+$EndSteps+" steps"
-            #Is this our shortest route so far?         
-            if ($global:fewestSteps -lt 1 -or $global:fewestSteps -gt $EndSteps){
-                $global:fewestSteps=$EndSteps
-                $global:bestPath=$path
-                " Shortest Route is now "+$global:fewestSteps #Display the change
-            }else{
-                " Shortest Route is still "+$global:fewestSteps #Display the result is unchanged
-            }
+    ($inputFile[$y][$x] -eq "b" ) -and (
+    ($X -gt 0 -and $inputFile[$y][$x-1] -eq "a") -or
+    ($X -lt $inputFile[0].length -and $inputFile[$y][$x+1] -eq "a") -or
+    ($y -lt $inputFile.Count-1 -and $inputFile[$y+1][$x] -eq "a") -or
+    ($y -gt 0 -and $inputFile[$y-1][$x] -eq "a")) ){
+        #Reached the end, no point looking for more steps down this route
+        $EndSteps=[int](getShortestPath $x $y)+1
+        "Reached the end in "+$EndSteps+" steps"
+        #Is this our shortest route so far?         
+        if ($global:fewestSteps -lt 1 -or $global:fewestSteps -gt $EndSteps){
+            $global:fewestSteps=$EndSteps
+            " Shortest Route is now "+$global:fewestSteps #Display the change
         }else{
-            #Look in each direction for something thats one step up, the same height or (some) down 
-            #or special case we're on an "S"-83 
-            #TODO from part 1 handling S right- it has an elevation "a". Works in example and my input because all adjacent are a or b though.
+            " Shortest Route is still "+$global:fewestSteps #Display the result is unchanged
+        }
+    }else{
+        #Optimisation- no point going further if we're already over the number of steps in the previous best route.
+        if ([int](getShortestPath $x $y) -lt $global:fewestSteps) {
+            #Look in each direction for something thats one step down, the same height or (some) up 
             #Left (x-1)
-            if($X -gt 0 -and (([byte][char]$inputFile[$y][$x-1])-$currentChar -le 1 -or $currentChar -eq 83)){
-                #Destination is at most one higher than current
+            if($X -gt 0 -and (([byte][char]$inputFile[$y][$x-1])-$currentChar -ge -1 )){
+                #Destination is at most one lower than current
                 #Update the shortest path to destination
                 if(setShortestPath ($x-1) $y ((getShortestPath $x $y)+1)){
                     #Go to that location if we have a new shortest route (or first time visit).
-                    stepTo ($x-1) $y $path
+                    stepTo ($x-1) $y 
                 }
             }
             #Right (x+1)
-            if($X -lt $inputFile[0].length -and (([byte][char]$inputFile[$y][$x+1])-$currentChar -le 1 -or $currentChar -eq 83)){
-                #Destination is at most one higher than current
+            if($X -lt $inputFile[0].length -and (([byte][char]$inputFile[$y][$x+1])-$currentChar -ge -1 )){
+                #Destination is at most one lower than current
                 #Update the shortest path to destination
                 if(setShortestPath ($x+1) $y ((getShortestPath $x $y)+1)){
                     #Visit the destination
-                    stepTo ($x+1) $y $path
+                    stepTo ($x+1) $y 
                 }
             }
             #Down (y+1)
-            if($y -lt $inputFile.Count-1 -and (([byte][char]$inputFile[$y+1][$x])-$currentChar -le 1 -or $currentChar -eq 83)){
-                #Destination is at most one higher than current
+            if($y -lt $inputFile.Count-1 -and (([byte][char]$inputFile[$y+1][$x])-$currentChar -ge -1 )){
+                #Destination is at most one lower than current
                 #Update the shortest path to destination
                 if(setShortestPath ($x) ($y+1) ((getShortestPath $x $y)+1)){
                     #Visit the destination
-                    stepTo ($x) ($y+1) $path
+                    stepTo ($x) ($y+1) 
                 }
             }
             #Up (y-1)
-            if($y -gt 0 -and (([byte][char]$inputFile[$y-1][$x])-$currentChar -le 1 -or $currentChar -eq 83)){
-                #Destination is at most one higher than current
+            if($y -gt 0 -and (([byte][char]$inputFile[$y-1][$x])-$currentChar -ge -1 )){
+                #Destination is at most one lower than current
                 #Update the shortest path to destination
                 if(setShortestPath ($x) ($y-1) ((getShortestPath $x $y)+1)){
                     #Visit the destination
-                    stepTo ($x) ($y-1) $path
+                    stepTo ($x) ($y-1)
                 }
             }
         }
+    }
     
 }
 
 #Keep track of the shortest route
-$global:fewestSteps=0
-$global:bestPath=""
-$path="" #Keep track of our route
-stepTo $currentX $currentY $path
+$global:fewestSteps=$inputFile[0].Length * $inputFile.count #Worst case scenario, we visit everything!
+stepTo $currentX $currentY 
 " "
 #$shortestPath #Show a table with the shortest route to each coordinate
 "Shortest route is: "+$global:fewestSteps
-#TODO: from part 1 answer is right, feel there could be more optimisation of the algorithm though.
-"Route was:"+ $global:bestPath
-$global:bestPath.trim("|").split("|") | select-object @{N="x";E={[int]$_.split(",")[0]}},@{N="y";E={[int]$_.split(",")[1]}} |% {$counter = 0} {$counter++; $_ | Add-Member -Name id -Value $counter -MemberType NoteProperty -PassThru}| Select-Object id, x, y, @{N="letter";E={$inputFile[$_.y][$_.x]}}
-#This doesn't provide the correct answer, but instead answers the question: what's the last point at height "a" in the shortest route that starts at S. 
-#ignoring this and rethinking, but committing for posterity because it's funky.
+
+
